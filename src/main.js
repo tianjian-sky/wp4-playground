971bb89227ef14c81a0dfdbe5cb729034fdf1fe9
tianjian-sky <532861047@qq.com>
2018-04-22 21:45:23 +0800

    自动拆包 chunkhash

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import './globalStyle/global.less'
 import  { a } from './modules/a'
 import  { b } from './modules/b'
+import $ from 'npm-zepto'
+
 let hello = 'hello world'
 document.getElementById('root').innerHTML = hello
 a.say()
 b.say()
+console.log($)
--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,14 @@ module.exports = {
     },
     output: {
         path: rootPath + '/dist/',
-        filename: '[name].js?[chunkhash]'
+        // filename: '[name].[chunkhash].js?'
+        filename: '[name].js?[chunkhash]'  // 此种方式会在src链接后加上chunkhash参数，而文件名固定  保持目录干净
     },
     resolve: {
     },
     plugins: [
-        new ExtractTextPlugin('style.[chunkhash].css'),  // extractTextPlugin 使用chunkhash
+        // new ExtractTextPlugin('style.[chunkhash].css'),  // extractTextPlugin 使用chunkhash
+        new ExtractTextPlugin('style.css？[chunkhash]'),
         new HtmlWebpackPlugin({
             filename: 'main.html',
             template: './src/templates/main.html',
@@ -87,5 +89,24 @@ module.exports = {
             use: ['file-loader']
         }
         ]
+    },
+    optimization: {
+        /*
+        上面提到 chunkFilename 指定了 chunk 打包输出的名字，那么文件名存在哪里了呢？
+        它就存在引用它的文件中。这意味着一个 chunk 文件名发生改变，会导致引用这个 chunk 文件也发生改变。
+    
+        runtimeChunk 设置为 true, webpack 就会把 chunk 文件名全部存到一个单独的 chunk 中，
+        这样更新一个文件只会影响到它所在的 chunk 和 runtimeChunk，避免了引用这个 chunk 的文件也发生改变。
+        */
+        runtimeChunk: true,
+    
+        splitChunks: {
+          /*
+          默认 entry 的 chunk 不会被拆分
+          因为我们使用了 html-webpack-plugin 来动态插入 <script> 标签，entry 被拆成多个 chunk 也能自动被插入到 html 中，
+          所以我们可以配置成 all, 把 entry chunk 也拆分了
+          */
+          chunks: 'all'
+        }
     }
 }
